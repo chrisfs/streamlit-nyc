3fa0c6330f447c0e6bc59104c37ebb0ccdeb7fdd
Chris Stehlik <chrisfs@gmail.com>
2020-07-18 16:39:03 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import plotly.express as px
 
 DATE_TIME = "date/time"
 DATA_URL = (
-    "/path/to/Motor_Vehicle_Collisions_-_Crashes.csv"
+    "Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used "
-            "to analyze motor vehicle collisions in NYC 🗽💥🚗")
+            "to analyze motor vehicle collisions in NYC")
 
 
 @st.cache(persist=True)
